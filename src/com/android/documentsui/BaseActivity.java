4651cd28074811f1da2b1f1fdca9d56406d819ab
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-20 20:45:10 +0300

    fix NPE crash after activity state restoration
    configStore is not restored when the State object is unparcelled.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- src/com/android/documentsui/BaseActivity.java
+++ src/com/android/documentsui/BaseActivity.java
@@ -494,6 +494,7 @@ public abstract class BaseActivity
             if (DEBUG) {
                 Log.d(mTag, "Recovered existing state object: " + state);
             }
+            state.configStore = mConfigStore;
             return state;
         }
 
